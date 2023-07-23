05605140846189af78cf341ca66a203f81d2483e
joao <joaovictorrodrigues444@gmail.com>
2023-07-23 02:39:11 -0300

    Melhorando a qualidade do codigo

--- script.js
+++ script.js
@@ -1,42 +1,24 @@
 
 
-const title = document.querySelector("#title")
-const text = document.querySelector("#text")
-const button = document.querySelector("#button")
-const table = document.querySelector(".table")
+const title = document.querySelector("#title");
+const text = document.querySelector("#text");
+const button = document.querySelector("#button");
+const table = document.querySelector(".table");
+
 
 function addTitle() {
     if (title.value == "" || text.value == "") {
         alert("Preenche todos os campos")
     } else {
 
-        let contentTitle = title.value
-        let contentText = text.value
-        
+        let table = document.querySelector(".modal .table")?.cloneNode(true);
 
-        const widget = document.createElement("div")
-        const widgetTitle = document.createElement("div")
-        const widgetText = document.createElement("div")
-        const pTitle = document.createElement("p")
-        const pText = document.createElement("p")
+        table.querySelector(".widget_title").textContent = title.value;
+        table.querySelector(".widget_text").textContent = text.value;
 
-        table.append(widget)
-        widget.append(widgetTitle, widgetText)
-        widgetTitle.append(pTitle)
-        widgetText.append(pText)
-        pTitle.innerText = contentTitle
-        pText.innerText = contentText
-        
-
-        widget.classList.add("widget")
-        widgetTitle.classList.add("widget_title")
-        widgetText.classList.add("widget_text")
-
-        
-        title.value = ""
-        text.value = ""
-
-
+        document.querySelector(".viewList")?.appendChild(table);
+        title.value = "";
+        text.value = "";
     }
 }
 
